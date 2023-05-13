c43709472e47b1317c24ff71720fab72de2ea99d
gokulapriyandckap <bgokulapriyandckap@gmail.com>
2023-05-13 17:50:44 +0530

    Partially Wrote code

---
+++ array.php
@@ -0,0 +1,14 @@
+<?php
+
+function ArrayAscendingOrder($array){
+    $result = [];
+    for ($i=0; $i < count($array)-1; $i++){
+        echo $i;
+        if ($array[$i] < $array[$i+1] && $array[$i+1] <= count($array)) {
+            $value = $array[$i+1];
+            array_push($result, $value);
+        }
+    }
+    print_r($result);
+}
+ArrayAscendingOrder([1,2,3]);
